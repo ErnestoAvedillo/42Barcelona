3680768c4b62bcac5dcb9c15bbb9ec1964a1ff4d
eavedillo <eavedillo@yahoo.es>
2022-11-05 09:45:50 +0100

    modifications to pass francinete strict

--- printf/entrega/ft_printf.c
+++ printf/entrega/ft_printf.c
@@ -47,19 +47,56 @@ static int	print_var(char *str, va_list args, int pos )
 	return (longitud);
 }
 
+int specials (char *str, int pos)
+{
+	char	*aux;
+	int		out;
+
+	printf("paso\n");
+	if (ft_isdigit (str[pos + 1]))
+	{
+		aux = ft_substr (str, pos, 3);
+		if (!aux)
+			return (-1);
+		out += write (1, &out, 4);
+		free (aux);
+	}
+	else if (str[pos + 1] == 'b' || str[pos + 1] == 't' \
+		 || str[pos + 1] == 'n' || str[pos + 1] == 'f' \
+		 || str[pos + 1] == 'r')
+	{
+		aux = ft_substr (str, pos, 2);
+		if (!aux)
+			return (-1);
+		out += write (1, &aux, 2);
+		free (aux);
+	}
+	else 
+		return (-1);
+	return (out);
+}
+
 int	ft_printf(char const *str, ...)
 {
 	va_list	args;
 	int		i;
 	int		val_ret;
+	int		aux;
 
 	va_start(args, str);
 	i = 0;
 	val_ret = 0;
 	while (str[i])
 	{
+		printf("%d\n",str[i]);
 		if (str[i] == '%')
 			val_ret += print_var ((char *)str, args, ++i);
+		else if (str[i] == 92)
+		{
+			aux = specials ((char *)str, i);
+			val_ret += aux;
+			i += aux - 1;
+		}
 		else
 			val_ret += write (1, &str[i], 1);
 		i++;
