2cfa2c3daf9711624e5efe88a80b616d2d67ffd5
Ernesto Avedillo Carretero <eavedill@car1s6.42barcelona.com>
2023-02-04 14:08:39 +0100

    added libft in fractol

---
+++ fract_ol/entrega/libft/ft_itoa.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eavedill <eavedill@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/15 18:10:40 by eavedill          #+#    #+#             */
+/*   Updated: 2022/09/24 11:20:17 by eavedill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include"libft.h"
+
+static int	getchrnr(long val)
+{
+	int	i;
+	int	aux;
+
+	aux = val;
+	i = 0;
+	if (val == 0)
+		return (1);
+	if (aux < 0)
+	{
+		i++;
+		aux *= -1;
+	}
+	while (aux > 0)
+	{
+		aux = aux / 10;
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		lenstr;
+
+	if (n == -2147483648)
+		return (ft_strdup ("-2147483648"));
+	lenstr = getchrnr (n);
+	str = (char *)malloc ((lenstr + 1) * sizeof (char));
+	if (!str)
+		return (NULL);
+	str[lenstr] = '\0';
+	if (n == 0)
+		str[0] = '0';
+	else
+	{
+		if (n < 0)
+			n *= -1;
+		str[0] = '-';
+		while (n > 0)
+		{	
+			str[--lenstr] = n % 10 + 48;
+			n = n / 10;
+		}
+	}
+	return (str);
+}
---
+++ fract_ol/entrega/libft/ft_lstiter.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eavedill <eavedill@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/15 18:10:18 by eavedill          #+#    #+#             */
+/*   Updated: 2022/09/18 17:33:46 by eavedill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include"libft.h"
+
+void	ft_lstiter(t_list *lst, void ( *f)(void *))
+{
+	if (!f || !lst)
+		return ;
+	while (lst->next != NULL)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+	f(lst->content);
+	return ;
+}
