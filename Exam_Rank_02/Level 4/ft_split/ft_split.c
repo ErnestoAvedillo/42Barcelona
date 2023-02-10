1d7d2bd5d6a2b2c874a8ed38185c21633a7a468b
Ernesto Avedillo <eavedillo@flexngate.com>
2023-02-10 20:21:56 +0100

    added exercise

--- Exam_Rank_02/Level 4/ft_split/ft_split.c
+++ Exam_Rank_02/Level 4/ft_split/ft_split.c
@@ -1,19 +1,23 @@
+#include<stdlib.h>
+#include<stdio.h>
+
 int is_space_or_tab (char c)
 {
-    if (c == ' ' || c ='\t')
+    if (c == ' ' || c == '\t')
         return (1);
-    else(0);
+    return(0);
 }
 int wordcount(char *str)
 {
     int i;
+    int n;
 
     i = 0;
     if (!is_space_or_tab(str[0]))
         n = 1;
     while(str[++i])
     {
-        if (str[i] && (is_space_or_tab(str[i]) && !is_space_or_tab(str[i+1])))
+        if (str[i] && (is_space_or_tab(str[i]) && !is_space_or_tab(str[i + 1])))
             n++;
     }
     return (n);
@@ -21,22 +25,52 @@ int wordcount(char *str)
 char    **ft_split(char *str)
 {
     int i;
+    int k;
     int n;
     int start;
     char **out;
 
     n = wordcount(str);
     i = 0;
-    *out = (char*) malloc((n + 1) * sizeof(char))
+    k = -1;
+    out = (char**) malloc((n + 1) * sizeof(char));
+    out[n + 1] = NULL;
     while(str[i])
     {
-        while (is_space_or_tab(str[i]) && str[i])
+        while (str[i] && is_space_or_tab(str[i]) )
             i++;
-        if(!is_space_or_tab(str[i]) && str[i])
+        if(str[i] && !is_space_or_tab(str[i]))
             start = i;
-        if(is_space_or_tab(str[i]) && start <= i)
+        while (str[i] && !is_space_or_tab(str[i]))
             i++;
-        else if()
+        if (start != i)
+        {
+            k++;
+            out[k] = (char *)malloc ((i - start + 1)* sizeof (char));
+            n = -1;
+            while (start <= i)
+            {
+                out[k][++n] = str[start];
+                start++;
+            }
+            out[k][n] = '\0';
+        }
     }
-    return(str);
+    return(out);
+}
+
+int main (int av, char **ac)
+{
+    int i = 0;
+    char **str;
+    if (av == 2)
+    str = ft_split (ac[1]);
+    printf ("era %s\n", ac[1]);
+    while (str[i] != NULL)
+    {
+        printf("spliteado %i --- %p ---- %s\n", i, str[i], str[i]);
+        i++;
+    }
+    printf("paso\n");
+    free (str);
 }
