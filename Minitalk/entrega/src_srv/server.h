6d081cd40614feec41841b04c59a824f0b976974
Ernesto Avedillo <eavedillo@flexngate.com>
2023-02-23 16:38:03 +0100

    intermediate backup

--- Minitalk/entrega/src_srv/server.h
+++ Minitalk/entrega/src_srv/server.h
@@ -2,4 +2,5 @@
 # define SERVER_H
 # include"../printf/ft_printf_bonus.h"
 # include"../nextline/get_next_line.h"
+# include"signal.h"
 #endif
--- Minitalk/entrega/srcs/server.c
+++
@@ -1,7 +0,0 @@
-#include"client.h"
-int main (int av, char **ac)
-{
-    int pid;
-    pid = getpid();
-    ft_printf("PID: %i \n",pid);
-}
