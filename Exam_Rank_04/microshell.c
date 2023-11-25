c6b438381549ac7f1f65513e962e08e32a79d02c
Ernesto Avedillo <eavedillo@yahoo.es>
2023-11-25 19:25:21 +0100

    nuevas pruebas

--- Exam_Rank_04/microshell.c
+++ Exam_Rank_04/microshell.c
@@ -6,7 +6,7 @@
 /*   By: eavedill <eavedill@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 16:03:45 by eavedill          #+#    #+#             */
-/*   Updated: 2023/11/19 18:41:19 by eavedill         ###   ########.fr       */
+/*   Updated: 2023/11/25 15:22:22 by eavedill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
@@ -20,16 +20,16 @@ void ft_putstr_err(char *str1, char *str2)
 {
 	int i = -1;
 	while (str1[++i])
-		write(2, str1[i], 1);
+		write(2, &str1[i], 1);
 	i = -1;
 	if (str2)
 		while (str2[++i])
-			write(2, str2[i], 1);
+			write(2, &str2[i], 1);
 }
 int len_arr(char **arr)
 {
 	int i = -1;
-	while(arr[i++]);
+	while(arr[++i]);
 	return i;
 }
 int ft_lenstr(char *str)
@@ -88,27 +88,16 @@ int my_cd(char **ac)
 	out = chdir(ac[1]);
 	if (out == -1)
 		printf("error: cd: cannot change directory to path_to_change\n");
-	else
-		printf("success: cd: %i changed to path \n", out);
 	return out;
 }
 
 
 int exec_cmd(char **ac, char **env, int tmp_fd)
 {
-	int pid;
-	int status;
-
-	pid = fork();
-	if (pid == -1)
-	{
-		printf("Fork error\n");
-		return 1;
-	}
-	else if (pid == 0)
-		execve(ac[0],ac,env);
+	dup2(tmp_fd,STDIN_FILENO);
 	close(tmp_fd);
-	wait(&status);
+	execve(ac[0],ac,env);
+	ft_putstr_err("error: cannot execute", ac[0]);
 	return 0;
 }
 
@@ -149,11 +138,10 @@ void prt_cmd(char **ac)
 int main (int av, char **ac, char **env)
 {
 	char **comando;
-	int is_semicol = 0;
-	int is_pipe= 0;
 	int i,j;
 	int tmp_fd, fd[2];
-
+	int status;
+	
 	if (av < 2)
 		return (0);
 	tmp_fd = dup(STDIN_FILENO);
@@ -161,24 +149,26 @@ int main (int av, char **ac, char **env)
 	while (++i <= av)
 	{
 		j = get_nxt_cmd(i,ac);
-		printf("resultado start %i end %i\n",i, j);
 		comando = ext_nxt_cmd(i,j,ac);
 		i = j;
-		prt_cmd(comando);
-		if(is_cd(comando))
+		if(strcmp(comando[0], "cd") == 0 || strcmp(comando[0], "/usr/bin/cd") == 0)
 		{
-			printf("El comando >%s< es CD\n", comando[0]);
 			my_cd(comando);
 		}
-		else
+		else if (!ac[i] || strcmp(ac[i], ";") == 0)
 		{
-			printf("ejecuto comando alternativo. >%s<\n", comando[0]);
-			exec_cmd(comando, env,tmp_fd);
+			if (fork() == 0)
+			{
+				exec_cmd(comando, env,tmp_fd);
+				return (1);
+			}
+			close(tmp_fd);
+			wait(&status);
 			tmp_fd = dup(STDIN_FILENO);
 		}
-		if (i != 1 && ac[i] && ac[i] == "|")
+		if (ac[i] && ac[i][0] == '|')
 		{
-			pipe(fd1)
+			pipe(fd);
 			if (fork() == 0)
 			{
 				dup2(fd[1], STDOUT_FILENO);
@@ -190,7 +180,7 @@ int main (int av, char **ac, char **env)
 			{
 				close (fd[1]);
 				close(tmp_fd);
-				tmp_fd = fd[0]
+				tmp_fd = fd[0];
 			}
 		}
 
