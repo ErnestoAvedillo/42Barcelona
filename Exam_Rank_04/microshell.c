64f0b526488ee2c59f7c4201a5c558d2cc9291ee
Ernesto Avedillo <eavedillo@yahoo.es>
2023-11-26 16:38:02 +0100

    pass ok exam

--- Exam_Rank_04/microshell.c
+++ Exam_Rank_04/microshell.c
@@ -5,186 +5,113 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: eavedill <eavedill@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/19 16:03:45 by eavedill          #+#    #+#             */
-/*   Updated: 2023/11/25 15:22:22 by eavedill         ###   ########.fr       */
+/*   Created: 2023/11/26 10:40:10 by eavedill          #+#    #+#             */
+/*   Updated: 2023/11/26 12:18:38 by eavedill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
-void ft_putstr_err(char *str1, char *str2)
+
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <unistd.h>
+ #include <string.h>
+ #include <sys/wait.h>
+ #include <sys/types.h>
+
+int prt_err(char *ac1, char *ac2)
 {
 	int i = -1;
-	while (str1[++i])
-		write(2, &str1[i], 1);
+	while (ac1[++i])
+		write(2, &ac1[i], 1);
 	i = -1;
-	if (str2)
-		while (str2[++i])
-			write(2, &str2[i], 1);
-}
-int len_arr(char **arr)
-{
-	int i = -1;
-	while(arr[++i]);
-	return i;
-}
-int ft_lenstr(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return i;
+	if (ac2)
+		while (ac2[++i])
+			write(2, &ac2[i], 1);
+	write(2, "\n", 1);
+	return 1;
 }
 
-char *ft_substr(char *str, int start, int len)
+int exec_cmd(char **ac, int i, int tmp_fd, char **env)
 {
-	char *out;
-	int i = -1;
-
-	if (!str)
-		return NULL;
-	if (start > ft_lenstr(str))
-		return NULL;
-	if (ft_lenstr(str) < len + start)
-		len = ft_lenstr(str) - start;
-	out = (char*) malloc((len + 1) * sizeof(char)) ;
-	while (++i <= len)
-		out[i] = str[start + i];
-	out[i] = '\0';
-	return out;
-}
-
-int is_cd(char **str)
-{
-	int i = 0;
-	char *command;
-	
-	i =  ft_lenstr(str[0]);
-	command = ft_substr(str[0], i -3, 3);
-	printf("Long str %i, comando es %s\n",i, command);
-	if (strncmp(str[0],"cd ",2) == 0 || strcmp(command, "/cd") == 0)
-		i = 1;
-	else
-		i= 0;
-	free (command);
-	return i;
-}
-
-int my_cd(char **ac)
-{
-	int out;
-
-	if (len_arr(ac) < 2)
-	{
-		printf("error: cd: bad arguments\n");
-		return -1;
-	}
-	out = chdir(ac[1]);
-	if (out == -1)
-		printf("error: cd: cannot change directory to path_to_change\n");
-	return out;
-}
-
-
-int exec_cmd(char **ac, char **env, int tmp_fd)
-{
-	dup2(tmp_fd,STDIN_FILENO);
+	dup2(tmp_fd, STDIN_FILENO);
 	close(tmp_fd);
-	execve(ac[0],ac,env);
-	ft_putstr_err("error: cannot execute", ac[0]);
-	return 0;
-}
-
-int get_nxt_cmd(int cur,char **ac)
-{
-	int i = cur;
-	while (ac[i] && ac[i][0] != '|' &&  ac[i][0] != ';')
-	{
-		i++;
-	}
-	return i;
-}
-char **ext_nxt_cmd(int start, int end, char **ac)
-{
-	char **out;
-	int i;
-	i = start;
-	out = (char**)malloc((end - start + 1) * sizeof(char*));
-	while (i < end)
-	{
-		out[i-start] = ac[i];
-		i++;
-	}
-	out[i-start] = NULL;
-	return out;
-}
-
-void prt_cmd(char **ac)
-{
-	int i = -1;
-	printf("Nueva lista de comandos.\n");
-	while (ac[++i])
-	{
-		printf("comando N.: %i es %s.\n",i,ac[i]);
-	}
+	ac[i] = NULL;
+	execve(ac[0], ac, env);
+	return (prt_err("error: cannot execute ", ac[0]));
 }
 
 int main (int av, char **ac, char **env)
 {
-	char **comando;
-	int i,j;
 	int tmp_fd, fd[2];
+	int i = 0;
 	int status;
 	
 	if (av < 2)
-		return (0);
+		return 0;
 	tmp_fd = dup(STDIN_FILENO);
-	i = 0;
-	while (++i <= av)
+	ac = &ac[1];
+//		printf("paso %i %s \n", i, ac[i]);
+	while (ac[i])
 	{
-		j = get_nxt_cmd(i,ac);
-		comando = ext_nxt_cmd(i,j,ac);
-		i = j;
-		if(strcmp(comando[0], "cd") == 0 || strcmp(comando[0], "/usr/bin/cd") == 0)
+		while(ac[i] && strcmp(ac[i],";") && strcmp(ac[i],"|"))
 		{
-			my_cd(comando);
+//		printf("paso %i %s \n", i, ac[i]);
+//		getchar();
+			i++;
 		}
-		else if (!ac[i] || strcmp(ac[i], ";") == 0)
+//		printf("salgo %i %s \n", i, ac[i]);
+		if(i > 0 && (!strcmp(ac[0], "cd") || !strcmp(ac[0], "/usr/bin/cd")))
 		{
+//			printf("es cd %i %s \n", i, ac[0]);
+			if (i != 2)
+				prt_err("error: cd: bad arguments",NULL);
+			else if (chdir(ac[1]) != 0)
+				prt_err("error: cd: cannot change directory to ", ac[1]);
+		}
+		else if (i > 0 &&(!ac[i] || !strcmp(ac[i], ";")))
+		{
+//			printf("es ; %i %s \n", i, ac[i]);
 			if (fork() == 0)
 			{
-				exec_cmd(comando, env,tmp_fd);
-				return (1);
-			}
-			close(tmp_fd);
-			wait(&status);
-			tmp_fd = dup(STDIN_FILENO);
-		}
-		if (ac[i] && ac[i][0] == '|')
-		{
-			pipe(fd);
-			if (fork() == 0)
-			{
-				dup2(fd[1], STDOUT_FILENO);
-				close(fd[0]);
-				close(fd[1]);
-				exec_cmd(comando, env,tmp_fd);
+				if(exec_cmd(ac,i,tmp_fd,env))
+					return 1;
 			}
 			else
 			{
-				close (fd[1]);
+				close(tmp_fd);
+				wait(&status);
+				tmp_fd = dup(STDIN_FILENO);
+			}
+		}
+		else if (!strcmp(ac[i], "|"))
+		{
+//			printf("es pipe %i %s \n", i, ac[0]);
+			pipe(fd);
+			if(fork() == 0 )
+			{
+				dup2(fd[1],STDOUT_FILENO);
+				close(fd[0]);
+				close(fd[1]);
+				if(exec_cmd(ac,i,tmp_fd, env))
+					return (1);
+			}
+			else
+			{
+				close(fd[1]);
 				close(tmp_fd);
 				tmp_fd = fd[0];
 			}
 		}
-
-
-		free(comando);
+//		printf("sigo en bucle %i del todo %s \n", i, ac[i]);
+		if(ac[i])
+			ac = &ac[i+1];
+		else
+			ac = &ac[i];
+//		printf("sigo en bucle %i del todo %s \n", i, ac[0]);
+		i = 0;
 	}
+//	printf("salgo %i del todo %s \n", i, ac[i]);
+	return (0);
 }
+
+
