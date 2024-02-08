dda9217d714289f5bab3d7ec33c34c0898428206
eavedill <eavedillo@yahoo.es>
2024-02-08 08:06:44 +0100

    Start cpp08

---
+++ CPP08/ex00/inc/main.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eavedill <eavedill@student.42barcelona>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/07 16:34:10 by eavedill          #+#    #+#             */
+/*   Updated: 2024/02/07 16:37:18 by eavedill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_HPP
+#define MAIN_HPP
+#include <iostream>
+template <typename T, int n>
+easyfind(T,n)
+{
+
+}
+#endif
