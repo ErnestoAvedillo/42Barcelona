8347d01b154eb085917d3b4177ee8a08d2fe7936
Ernesto Avedillo <eavedillo@yahoo.es>
2024-02-08 15:39:45 +0100

    finish CPP08 ex00

--- CPP08/ex00/inc/main.hpp
+++ CPP08/ex00/inc/main.hpp
@@ -3,19 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   main.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eavedill <eavedill@student.42barcelona>    +#+  +:+       +#+        */
+/*   By: eavedill <eavedill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 16:34:10 by eavedill          #+#    #+#             */
-/*   Updated: 2024/02/07 16:37:18 by eavedill         ###   ########.fr       */
+/*   Updated: 2024/02/08 15:31:42 by eavedill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAIN_HPP
 #define MAIN_HPP
 #include <iostream>
-template <typename T, int n>
-easyfind(T,n)
-{
+#include <vector>
+#include <list>
+#include <deque>
+#include <string>
 
+template < typename T>
+typename T::iterator easyfind(T &v,int N)
+{
+	typename T::iterator pos =  std::find(v.begin(), v.end(), N);
+	if (pos != v.end())
+		return (pos);
+	throw std::runtime_error("Value not found\n");
 }
 #endif
+
+template <typename T>
+void test (T & val, int to_find,std::string text)
+{
+	std::cout << text << std::endl;
+	srand((unsigned int)time((time_t *)NULL));
+	for (int i = 0; i < 20; i++)
+		val.push_back(rand() % to_find);
+	int target = rand() % to_find;
+	try
+	{
+		typename T::iterator i = easyfind(val, rand() % to_find);
+		std::cout << "Value " << target << " found at pos : " << (i - val.begin()) <<  std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Target " << target << " " << e.what() << '\n';
+	}
+}
--- CPP08/ex00/src/main.cpp
+++ CPP08/ex00/src/main.cpp
@@ -3,16 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eavedill <eavedill@student.42barcelona>    +#+  +:+       +#+        */
+/*   By: eavedill <eavedill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 16:33:32 by eavedill          #+#    #+#             */
-/*   Updated: 2024/02/07 16:35:21 by eavedill         ###   ########.fr       */
+/*   Updated: 2024/02/08 15:37:32 by eavedill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/main.hpp"
 
-int main(void)
+int main(int av, char **ac)
 {
-	
+	int to_find;
+	if (av < 2)
+		to_find = 5;
+	else
+	{
+		try
+		{
+			std::string value(ac[1]);
+			to_find = std::stoi(value);	
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << "No conversion to int possible Bye." << '\n';
+			std::cerr << e.what() << '\n';
+			return 0;
+		}
+	}	
+	std::vector<int> val;
+	test (val, to_find, "Test with vectors");
+	std::deque<int> val2;
+	test (val2, to_find, "Test with deque");
+	std::list<int> val3;
+	test (val2, to_find, "Test with deque");
 }
