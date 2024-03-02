fe131c7f725969f04b3d9703120379a154276022
Ernesto Avedillo <eavedillo@yahoo.es>
2024-03-02 11:10:18 +0100

    start ex02

---
+++ CPP09/ex02/inc/main.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/01 07:35:32 by marvin            #+#    #+#             */
+/*   Updated: 2024/03/01 07:35:32 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_HPP
+#define MAIN_HPP
+#include <iostream>
+#include <deque>
+#include <sstream>
+
+int convert (std::string str);
+
+#endif
